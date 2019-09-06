44cecb5dd632ca6b329ddb04e77c23d8e6e2a9bd
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-09-06 13:07:58 -0300

    changed settings structure, added environments variables

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # revenue_latam
+
+[![Build Status](https://travis-ci.com/evbeda/revenue_latam.svg?branch=master)](https://travis-ci.com/evbeda/revenue_latam)
+
+[![Coverage Status](https://coveralls.io/repos/github/evbeda/revenue_latam/badge.svg)](https://coveralls.io/github/evbeda/revenue_latam)
---
+++ settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+from .utils import get_env_variable
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+SOCIAL_AUTH_EVENTBRITE_KEY = get_env_variable('SOCIAL_AUTH_EVENTBRITE_KEY')
+SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable(
+    'SOCIAL_AUTH_EVENTBRITE_SECRET',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
