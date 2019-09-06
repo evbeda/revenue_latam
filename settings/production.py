645e60c802efb231a71b2f82498a448bb17ba896
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-09-06 13:07:58 -0300

    changed settings structure, added environments variables

---
+++ settings/production.py
@@ -0,0 +1,26 @@
+# import dj_database_url
+from .base import *
+from .utils import get_env_variable
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+SOCIAL_AUTH_EVENTBRITE_KEY = get_env_variable('SOCIAL_AUTH_EVENTBRITE_KEY')
+SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable(
+    'SOCIAL_AUTH_EVENTBRITE_SECRET',
+)
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'revenue',
+#         'USER': 'name',
+#         'PASSWORD': '',
+#         'PORT': '',
+#     }
+# }
+#
+# DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
+#
+# DATABASES['default'].update(DB_FROM_ENV)
