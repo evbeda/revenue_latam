5690db950751b6daef16a7305a620f0ca474303d
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-09-06 11:18:25 -0300

    adding travis and coveralls

--- revenue_latam/settings.py
+++ revenue_latam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from . import get_env_variable
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tes5j691y^y&9^c#yjzb802u$bru)ue-2tm$+2^@y_9t*5_(4r'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
