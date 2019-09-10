323d4930070bd91cd41a171aba2d2605db7dbd1e
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-09-10 12:24:02 -0300

    adding chart js and some chart examples

--- revenue_app/urls.py
+++ revenue_app/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
-from revenue_app.views import Dashboard
+
+from revenue_app.views import (
+    ChartJSONDataView,
+    ChartJSONDataViewAlt,
+    Dashboard,
+)
 
 
 urlpatterns = [
     url(r'^$', Dashboard.as_view(), name='dashboard'),
+    url(r'^json/$', ChartJSONDataView.as_view(), name='json_data'),
+    url(r'^json_alt/$', ChartJSONDataViewAlt.as_view(), name='json_data_alt'),
 ]
--- revenue_app/views.py
+++ revenue_app/views.py
@@ -1,5 +1,56 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from chartjs.views.lines import BaseLineChartView
 
-class Dashboard(TemplateView):
+
+class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'revenue_app/dashboard.html'
+
+
+class ChartJSONDataView(BaseLineChartView):
+    def get_providers(self):
+        '''
+        Return names of dataset.
+        Returns [] in parent class.
+        '''
+        return ['Org1', 'Org2', 'Org3']
+
+    def get_labels(self):
+        '''
+        Return labels for the x-axis.
+        Not implemented in parent class.
+        '''
+        return ['GTS', 'GTF', 'Qty']
+
+    def get_data(self):
+        '''
+        Return lists of datasets to show.
+        Example: [[1, 2, 3], [4, 5, 6]].
+        Not implemented in parent class.
+        '''
+        return [[5, 5, 5], [6, 5, 4], [4, 5, 6]]
+
+
+class ChartJSONDataViewAlt(BaseLineChartView):
+    def get_providers(self):
+        '''
+        Return names of dataset.
+        Returns [] in parent class.
+        '''
+        return ['GTS', 'GTF', 'Qty']
+
+    def get_labels(self):
+        '''
+        Return labels for the x-axis.
+        Not implemented in parent class.
+        '''
+        return ['Org1', 'Org2', 'Org3']
+
+    def get_data(self):
+        '''
+        Return lists of datasets to show.
+        Example: [[1, 2, 3], [4, 5, 6]].
+        Not implemented in parent class.
+        '''
+        return [[5, 5, 5], [6, 5, 4], [4, 5, 6]]
