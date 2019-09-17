fbf8b59093bfa7d4f1dc1e57e03bd391a6a5bfb9
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-09-17 15:10:26 -0300

    Set project structure, add pandas and numpy, update gitignore

--- revenue_app/urls.py
+++ revenue_app/urls.py
@@ -4,11 +4,13 @@ from revenue_app.views import (
     ChartJSONDataView,
     ChartJSONDataViewAlt,
     Dashboard,
+    OrganizerList
 )
 
 
 urlpatterns = [
     url(r'^$', Dashboard.as_view(), name='dashboard'),
+    url(r'organizers/$', OrganizerList.as_view(), name='organizer-list'),
     url(r'^json/$', ChartJSONDataView.as_view(), name='json_data'),
     url(r'^json_alt/$', ChartJSONDataViewAlt.as_view(), name='json_data_alt'),
 ]
