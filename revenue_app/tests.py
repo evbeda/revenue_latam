b632d17866a647a01b3dc57e3d76056bbedc092a
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-09-18 09:49:39 -0300

    Load CSVs and filter by date

--- revenue_app/tests.py
+++ revenue_app/tests.py
@@ -1,3 +1,111 @@
+from datetime import date
 from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from pandas import read_csv
+from pandas.core.frame import DataFrame
+from parameterized import parameterized
+
+from .utils import (
+    filter_transactions_by_date,
+    get_organizer_sales,
+    get_transactions,
+)
+
+
+class UtilsTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_transactions(self):
+        with patch(
+            'pandas.read_csv',
+            return_value=read_csv('revenue_app/tests/transactions_example.csv')
+        ):
+            transactions = get_transactions()
+        self.assertIsInstance(
+            transactions,
+            DataFrame
+        )
+        self.assertListEqual(
+            transactions.columns.tolist(),
+            [
+                'eventholder_user_id',
+                'transaction_created_date',
+                'payment_processor',
+                'currency',
+                'event_id',
+                'email',
+                'sale__payment_amount__epp',
+                'sale__eb_tax__epp',
+                'sale__ap_organizer__gts__epp',
+                'sale__ap_organizer__royalty__epp',
+                'sale__gtf_esf__epp',
+                'sale__gtf_esf__offline',
+                'refund__payment_amount__epp',
+                'refund__gtf_epp__gtf_esf__epp',
+                'refund__ap_organizer__gts__epp',
+                'refund__eb_tax__epp'
+            ]
+        )
+        self.assertNotIn(
+            'sale__eb_tax__epp__1',
+            transactions.columns.tolist()
+        )
+        self.assertEqual(
+            len(transactions),
+            5
+        )
+
+    def test_get_organizer_sales(self):
+        with patch(
+            'pandas.read_csv',
+            return_value=read_csv('revenue_app/tests/organizer_sales_example.csv')
+        ):
+            organizer_sales = get_organizer_sales()
+        self.assertIsInstance(
+            organizer_sales,
+            DataFrame
+        )
+        self.assertListEqual(
+            organizer_sales.columns.tolist(),
+            [
+                'email',
+                'organizer_name',
+                'sales_flag',
+                'event_id',
+                'GTSntv',
+                'GTFntv',
+                'PaidTix'
+            ]
+        )
+        self.assertNotIn(
+            'organizer_email',
+            organizer_sales.columns.tolist()
+        )
+        self.assertEqual(
+            len(organizer_sales),
+            5
+        )
+
+    @parameterized.expand([
+        (date(2018, 8, 3), None, 1),
+        (date(2018, 8, 5), None, 2),
+        (date(2018, 8, 3), date(2018, 8, 16), 4),
+        (date(2018, 8, 5), date(2018, 8, 3), 0),
+    ])
+    def test_filter_transactions_by_date(self, start_date, end_date, expected_length):
+        with patch(
+            'pandas.read_csv',
+            return_value=read_csv('revenue_app/tests/transactions_example.csv')
+        ):
+            transactions = get_transactions()
+        filtered_transactions = filter_transactions_by_date(transactions, start_date, end_date)
+        self.assertIsInstance(
+            filtered_transactions,
+            DataFrame
+        )
+        self.assertEqual(
+            len(filtered_transactions),
+            expected_length
+        )
--- revenue_app/utils.py
+++ revenue_app/utils.py
@@ -1,20 +1,38 @@
+import numpy as np
 import pandas as pd
 
 
-# transactions_w_pp = pd.read_excel('datasets/Revenue Queries.xlsx', sheet_name=1, header=1, usecols=range(0,16))
-# transactions_w_email = pd.read_excel('datasets/Revenue Queries.xlsx', sheet_name=2, header=1, usecols=range(0,16))
-
 def get_rollups():
-    return pd.read_excel('datasets/Revenue Queries.xlsx', sheet_name=0, header=1, usecols=range(0,53))
+    return pd.read_excel('datasets/Revenue Queries.xlsx', sheet_name=0, header=1, usecols=range(0, 53))
 
 
 def get_transactions():
-    return pd.read_csv('datasets/transactions.csv')
+    transactions = pd.read_csv('datasets/transactions.csv').replace(np.nan, '', regex=True)
+    if 'sale__eb_tax__epp__1' in transactions.columns:
+        transactions.drop('sale__eb_tax__epp__1', axis=1, inplace=True)
+    transactions['transaction_created_date'] = pd.to_datetime(
+        transactions['transaction_created_date'],
+        format="%m/%d/%Y"
+    )
+    return transactions
 
 
 def get_organizer_sales():
-    return pd.read_csv('datasets/organizer_sales.csv')
+    organizer_sales = pd.read_csv('datasets/organizer_sales.csv').replace(np.nan, '', regex=True)
+    if 'organizer_email' in organizer_sales.columns:
+        organizer_sales.rename(columns={'organizer_email': 'email'}, inplace=True)
+    return organizer_sales
 
 
 def get_organizer_list():
     return []
+
+
+def filter_transactions_by_date(dataframe, start_date, end_date=None):
+    start_date = np.datetime64(start_date, 'D')
+    filter_condition = (dataframe['transaction_created_date'] == start_date)
+    if end_date:
+        end_date = np.datetime64(end_date, 'D')
+        filter_condition = (dataframe['transaction_created_date'] >= start_date) & \
+            (dataframe['transaction_created_date'] <= end_date)
+    return dataframe[filter_condition]
