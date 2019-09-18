bf78ff86a1262cdfc2f6cfd79649f13a0f3d4f29
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-09-18 12:39:44 -0300

    Add dates filter

--- revenue_app/urls.py
+++ revenue_app/urls.py
@@ -5,8 +5,9 @@ from revenue_app.views import (
     ChartJSONDataViewAlt,
     Dashboard,
     OrganizerEventList,
-    OrganizersTransactions,
     OrganizerTransactions,
+    OrganizersTransactions,
+    TransactionsByDate,
 )
 
 
@@ -14,6 +15,7 @@ urlpatterns = [
     url(r'^$', Dashboard.as_view(), name='dashboard'),
     url(r'transactions/$', OrganizersTransactions.as_view(), name='organizers-transactions'),
     url(r'organizers/search/$', OrganizerTransactions.as_view(), name='organizer-transactions'),
+    url(r'transactions/dates/$', TransactionsByDate.as_view(), name='transactions-by-dates'),
     url(r'organizers/(?P<organizer_id>[0-9]+)/events/$', OrganizerEventList.as_view(), name='organizer-event-list'),
     url(r'^json/$', ChartJSONDataView.as_view(), name='json_data'),
     url(r'^json_alt/$', ChartJSONDataViewAlt.as_view(), name='json_data_alt'),
