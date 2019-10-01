ca9d466b7e479d3c9cd9125f4c0b7b8f4d46006c
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-10-01 10:48:42 -0300

    Add info to Top Events screen

--- revenue_app/tests.py
+++ revenue_app/tests.py
@@ -143,7 +143,7 @@ class UtilsTestCase(TestCase):
             sorted(BASE_ORGANIZER_SALES_COLUMNS),
         )
         self.assertNotIn('organizer_email', organizer_sales.columns.tolist())
-        self.assertEqual(len(organizer_sales), 5)
+        self.assertEqual(len(organizer_sales), 27)
 
     def test_merge_transactions(self):
         merged_transactions = merge_transactions(self.transactions, self.organizer_sales)
@@ -249,7 +249,7 @@ class UtilsTestCase(TestCase):
         self.assertIsInstance(transactions_event, DataFrame)
         self.assertEqual(len(transactions_event), transactions_qty)
         self.assertIsInstance(paidtix, Series)
-        self.assertEqual(len(paidtix), 1)
+        self.assertEqual(len(paidtix), transactions_qty)
         self.assertEqual(paidtix.iloc[0], tickets_qty)
 
     def test_get_top_ten_organizers(self):
@@ -313,8 +313,8 @@ class UtilsTestCase(TestCase):
         top_brl = get_top_events(trx[trx['currency'] == 'BRL'])
         self.assertIsInstance(top_ars, DataFrame)
         self.assertIsInstance(top_brl, DataFrame)
-        self.assertEqual(len(top_ars), 2)
-        self.assertEqual(len(top_brl), 3)
+        self.assertEqual(len(top_ars), 3)
+        self.assertEqual(len(top_brl), 4)
 
     def test_random_color(self):
         color = random_color()
@@ -557,8 +557,8 @@ class ViewsTest(TestCase):
             read_csv(ORGANIZER_SALES_EXAMPLE_PATH),
         )):
             response = self.logged_client.get(URL)
-        self.assertEqual(len(response.context['top_event_ars']), 2)
-        self.assertEqual(len(response.context['top_event_brl']), 3)
+        self.assertEqual(len(response.context['top_event_ars']), 3)
+        self.assertEqual(len(response.context['top_event_brl']), 4)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.template_name[0], TopEventsLatam.template_name)
 
