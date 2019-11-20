91fc610d52b827d7e1dc6e9bb3fccf1a4929ed4e
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-11-20 16:31:17 -0300

    Fix exchange information shown in the navbar

--- revenue_app/context_processors.py
+++ revenue_app/context_processors.py
@@ -2,4 +2,5 @@ def context(request):
     return {
         'query_info': request.session.get('query_info'),
         'exchange_data': request.session.get('exchange_data'),
+        'class_exchange': request.session.get('class_exchange'),
     }
